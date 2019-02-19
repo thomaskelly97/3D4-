d6961f5be0c5ccc15449f2119b4b2775815172c0
Thomas Kelly <tkelly2@lg12-15.scss.tcd.ie>
2019-02-19 18:36:09 +0000

    Just started

---
+++ practical2.c
@@ -0,0 +1,92 @@
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include "cond.c"
+
+int pnum;  // number updated when producer runs.
+int csum;  // sum computed using pnum when consumer runs.
+
+int (*pred)(int); // predicate indicating number to be consumed
+
+int produceT() {
+  scanf("%d",&pnum);
+  return pnum;
+}
+
+void *Produce(void *a) {
+  int p;
+
+  p=1;
+  while (p) {
+    printf("producer thinking...\n");
+    sleep(1);
+    printf("..done!\n");
+    p = produceT();
+    printf("PRODUCED %d\n",p);
+  }
+  printf("EXIT-P\n");
+}
+
+
+int consumeT() {
+  if ( pred(pnum) ) { csum += pnum; }
+  return pnum;
+}
+
+void *Consume(void *a) {
+  int p;
+
+  p=1;
+  while (p) {
+    printf("consumer thinking...\n");
+    sleep(rand()%3);
+    printf("..done!\n");
+    p = consumeT();
+    printf("CONSUMED %d\n",csum);
+  }
+  printf("EXIT-C\n");
+}
+
+
+int main (int argc, const char * argv[]) {
+  // the current number predicate
+  static pthread_t prod,cons;
+	long rc;
+
+  pred = &cond1;
+  if (argc>1) {
+    if      (!strncmp(argv[1],"2",10)) { pred = &cond2; }
+    else if (!strncmp(argv[1],"3",10)) { pred = &cond3; }
+  }
+
+
+  pnum = 999;
+  csum=0;
+  srand(time(0));
+
+  printf("Creating Producer:\n");
+ 	rc = pthread_create(&prod,NULL,Produce,(void *)0);
+	if (rc) {
+			printf("ERROR return code from pthread_create(prod): %ld\n",rc);
+			exit(-1);
+		}
+  printf("Creating Consumer:\n");
+ 	rc = pthread_create(&cons,NULL,Consume,(void *)0);
+	if (rc) {
+			printf("ERROR return code from pthread_create(cons): %ld\n",rc);
+			exit(-1);
+		}
+
+	pthread_join( prod, NULL);
+	pthread_join( cons, NULL);
+
+
+  printf("csum=%d.\n",csum);
+
+  return 0;
+}
